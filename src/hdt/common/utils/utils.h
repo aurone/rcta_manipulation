2f25b01c356fd6900cafad4914aaebd203e2f0ae
Andrew Dornbush <andrew.dornbush@gmail.com>
2014-08-27 17:59:32 -0400

    add signf util

--- src/hdt/common/utils/utils.h
+++ src/hdt/common/utils/utils.h
@@ -7,5 +7,18 @@ const T& clamp(const T& t, const T& min, const T& max)
     return t > max ? max : t < min ? min : t;
 }
 
+double signf(double d, double eps = 0.0)
+{
+    if (d < eps) {
+        return -1.0;
+    }
+    else if (d > eps) {
+        return 1.0;
+    }
+    else {
+        return 0.0;
+    }
+}
+
 #endif
 
