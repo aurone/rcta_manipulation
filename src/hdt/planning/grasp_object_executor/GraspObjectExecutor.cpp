998dd8e43ac9ae6d84e2a9c4738e661327415564
Samarth <samarth.robo@gmail.com>
2014-10-07 18:14:19 -0400

    add joint names to joint goal requests from grasping pipeline
    Former-commit-id: 10bb920ddb17fc64628172841e3ea2bb7bbf0229

--- src/hdt/planning/grasp_object_executor/GraspObjectExecutor.cpp
+++ src/hdt/planning/grasp_object_executor/GraspObjectExecutor.cpp
@@ -723,6 +723,7 @@ int GraspObjectExecutor::run()
                 const StowPosition& next_stow_position = stow_positions_[next_stow_position_to_attempt_++];
 
                 last_move_arm_stow_goal_.type = hdt::MoveArmCommandGoal::JointGoal;
+                last_move_arm_stow_goal_.goal_joint_state.name = robot_model_->joint_names();
                 last_move_arm_stow_goal_.goal_joint_state.position = next_stow_position.joint_positions;
 
 //                last_move_arm_stow_goal_.type = hdt::MoveArmCommandGoal::EndEffectorGoal;
