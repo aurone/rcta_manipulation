88d3a240fb299c0e281f8c7d9fdc113235215266
Kalin Gochev <kalin.gochev@gmail.com>
2014-10-23 14:52:03 -0400

    Changing the way stow arm is going to work. Rather than a system call to roslaunch, they will send us a stow message on a string topic
    Former-commit-id: b543d1d7f38340d453da33b8bdd0f0d829c6a9a1

--- src/hdt/planning/stow_arm/stow_arm.cpp
+++ src/hdt/planning/stow_arm/stow_arm.cpp
@@ -6,17 +6,24 @@
 #include <hdt/MoveArmCommandAction.h>
 #include <hdt/common/hdt_description/RobotModel.h>
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 #include <hdt/common/msg_utils/msg_utils.h>
 #include <hdt/common/stringifier/stringifier.h>
 #include <hdt/common/utils/utils.h>
-
-bool cb_success = false;
 
 struct StowPosition
 {
 	std::string name;
 	std::vector<double> joint_positions;
 };
+
+bool cb_success = false;
+std::vector<StowPosition> stow_positions_;
+actionlib::SimpleActionClient<hdt::MoveArmCommandAction> *client_;
+hdt::RobotModelPtr robot_model;
+
+ros::Publisher pub;
+ros::Subscriber sub;
 
 bool extract_xml_value(
     XmlRpc::XmlRpcValue& value,
@@ -37,6 +44,7 @@ bool extract_xml_value(
     return success;
 }
 
+
 void result_callback(
 	const actionlib::SimpleClientGoalState& state,
 	const hdt::MoveArmCommandResult::ConstPtr& result)
@@ -46,6 +54,58 @@ void result_callback(
 		cb_success = true;
 	} else {
 		ROS_WARN("...");
+	}
+}
+
+
+bool try_stow_arm(){
+
+	if (!client_->waitForServer(ros::Duration(10.0))) {
+		return false;
+	}
+
+	hdt::MoveArmCommandGoal move_arm_stow_goal;
+	move_arm_stow_goal.type = hdt::MoveArmCommandGoal::JointGoal;
+	move_arm_stow_goal.goal_joint_state.name = robot_model->joint_names();
+	move_arm_stow_goal.octomap; // meh?
+    	//include the attached object in the goal
+    	move_arm_stow_goal.has_attached_object = false;
+    	move_arm_stow_goal.execute_path = true;
+
+	for(size_t i = 0; i < stow_positions_.size(); i++){
+		ROS_INFO("Trying stow position %s: %s", stow_positions_[i].name.c_str(), to_string(stow_positions_[i].joint_positions).c_str());
+
+		move_arm_stow_goal.goal_joint_state.position = stow_positions_[i].joint_positions;
+    			//std::vector<double>(robot_model->joint_names().size(), 0);
+
+		auto result_cb = boost::bind(result_callback, _1, _2);
+		client_->sendGoal(move_arm_stow_goal, result_cb);
+		if (!client_->waitForResult()) {
+			return false;
+		}
+		if(cb_success) return true;
+	}
+	//if we got here all stow positions failed!
+	ROS_WARN("All stow positions failed!");
+	return false;
+}
+
+void stow_request_callback(const std_msgs::String::ConstPtr& message){
+	if(message != NULL){
+		std::string msg = message->data;
+		ROS_INFO("Got message '%s'", msg.c_str());
+		if(msg.compare("stow")==0){
+			cb_success = false;
+			if(!try_stow_arm()){
+				std_msgs::String response;
+				response.data = std::string("error");
+				pub.publish(response);				
+			} else {
+				std_msgs::String response;
+				response.data = std::string("done");
+				pub.publish(response);
+			}
+		} 
 	}
 }
 
@@ -71,9 +131,7 @@ int main(int argc, char* argv[])
 		exit(FAILED_TO_RETRIEVE_ROBOT_DESCRIPTION);
 	}
 
-	hdt::RobotModelPtr robot_model = hdt::RobotModel::LoadFromURDF(urdf_string);
-
-	std::vector<StowPosition> stow_positions_;
+	robot_model = hdt::RobotModel::LoadFromURDF(urdf_string);
 
 	// read in stow positions
     	if (!msg_utils::download_param(ph, "stow_positions", stow_positions_)) {
@@ -89,35 +147,13 @@ int main(int argc, char* argv[])
 
 	const std::string move_arm_command_action_name = "move_arm_command";
 	actionlib::SimpleActionClient<hdt::MoveArmCommandAction> client(move_arm_command_action_name, true);
+	client_ = &client;
 
-	if (!client.waitForServer(ros::Duration(10.0))) {
-		exit(TIMED_OUT_WAITING_FOR_SERVER);
-	}
+	pub = nh.advertise<std_msgs::String>("stow_arm_response", 5);
+        sub = nh.subscribe("stow_arm_request", 1, stow_request_callback);
 
-	hdt::MoveArmCommandGoal move_arm_stow_goal;
-	move_arm_stow_goal.type = hdt::MoveArmCommandGoal::JointGoal;
-	move_arm_stow_goal.goal_joint_state.name = robot_model->joint_names();
-	move_arm_stow_goal.octomap; // meh?
-    	//include the attached object in the goal
-    	move_arm_stow_goal.has_attached_object = false;
-    	move_arm_stow_goal.execute_path = true;
-
-	for(size_t i = 0; i < stow_positions_.size(); i++){
-		ROS_INFO("Trying stow position %s: %s", stow_positions_[i].name.c_str(), to_string(stow_positions_[i].joint_positions).c_str());
-
-		move_arm_stow_goal.goal_joint_state.position = stow_positions_[i].joint_positions;
-    			//std::vector<double>(robot_model->joint_names().size(), 0);
-
-		auto result_cb = boost::bind(result_callback, _1, _2);
-		client.sendGoal(move_arm_stow_goal, result_cb);
-		if (!client.waitForResult()) {
-			exit(TIMED_OUT_WAITING_FOR_RESULT);
-		}
-		if(cb_success) return SUCCESS;
-	}
-	//if we got here all stow positions failed!
-	ROS_WARN("All stow positions failed!");
-	return FAILED_TO_STOW_ARM;
+	ROS_INFO("Initialized! Waiting on messages!");
+	ros::spin();
 	//return SUCCESS;
 }
 
