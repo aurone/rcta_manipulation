94e3d1f5d890ce57c9e56693d89dd95055d6121b
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-10-04 11:56:59 -0400

    split roman joint trajectory controller into header/source files
    Former-commit-id: 075ee1af0deccfb1bbd0c8036549d9088ad52b5f

--- src/rcta/src/rcta/control/roman_control/roman_joint_trajectory_controller.h
+++ src/rcta/src/rcta/control/roman_control/roman_joint_trajectory_controller.h
@@ -1,21 +1,57 @@
-#ifndef rcta_roman_joint_trajectory_controller_h
-#define rcta_roman_joint_trajectory_controller_h
+#ifndef ROMAN_JOINT_TRAJECTORY_CONTROLLER_H
+#define ROMAN_JOINT_TRAJECTORY_CONTROLLER_H
 
-namespace rcta {
+#include <map>
+#include <string>
+
+#include <actionlib/server/simple_action_server.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <roman_client_ros_utils/RomanSpecReply.h>
+#include <roman_client_ros_utils/RomanState.h>
+#include <ros/ros.h>
+
+#define BUILD_LIVE_JPL 0
 
 class RomanJointTrajectoryController
 {
 public:
 
     RomanJointTrajectoryController();
+
     ~RomanJointTrajectoryController();
 
     int run();
 
 private:
+
+    typedef actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction>
+    FollowJointTrajectoryActionServer;
+
+    ros::NodeHandle m_nh;
+
+    FollowJointTrajectoryActionServer m_server;
+
+    ros::Publisher m_roman_spec_pub;
+    ros::Subscriber m_roman_spec_reply_sub;
+    ros::Subscriber m_state_sub;
+
+    FollowJointTrajectoryActionServer::GoalConstPtr m_goal;
+    FollowJointTrajectoryActionServer::Feedback m_feedback;
+    FollowJointTrajectoryActionServer::Result m_result;
+
+    roman_client_ros_utils::RomanState::ConstPtr m_state;
+
+    std::map<std::string, int> m_joint_name_to_spec_index;
+
+    void romanStateCallback(
+        const roman_client_ros_utils::RomanState::ConstPtr& msg);
+
+    void goalCallback();
+
+    void preemptCallback();
+
+    void romanSpecReplyCallback(
+        const roman_client_ros_utils::RomanSpecReply::ConstPtr& msg);
 };
 
-} // namespace rcta
-
 #endif
-
