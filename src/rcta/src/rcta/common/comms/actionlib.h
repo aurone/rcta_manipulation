b684bbf508c8bb899ea2c36ef71b94dae50c0bc3
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-10-06 17:28:37 -0400

    add action client reconnect variant for shared pointers
    Former-commit-id: 68fab3f194a8d51b784ce51cece59bc8e3553690

--- src/rcta/src/rcta/common/comms/actionlib.h
+++ src/rcta/src/rcta/common/comms/actionlib.h
@@ -1,6 +1,9 @@
 #ifndef rcta_actionlib_h
 #define rcta_actionlib_h
 
+#include <memory>
+#include <string>
+
 namespace rcta {
 
 template <typename ActionType>
@@ -44,6 +47,46 @@ bool ReconnectActionClient(
     return false;
 }
 
+template <typename ActionType>
+bool ReconnectActionClient(
+    std::shared_ptr<actionlib::SimpleActionClient<ActionType>>& client,
+    const std::string& action_name,
+    ros::Rate poll_frequency = ros::Rate(10.0),
+    const ros::Duration& timeout = ros::Duration(1.0))
+{
+    if (!client) {
+        client.reset(new actionlib::SimpleActionClient<ActionType>(action_name, false));
+    }
+
+    ROS_DEBUG("Waiting for action server '%s'", action_name.c_str());
+
+    if (!client) {
+        ROS_WARN("Action client is null");
+        return false;
+    }
+
+    ros::Time start = ros::Time::now();
+    while (ros::ok() && (timeout == ros::Duration(0) || ros::Time::now() < start + timeout)) {
+        ros::spinOnce();
+        if (!client->isServerConnected()) {
+            client.reset(new actionlib::SimpleActionClient<ActionType>(action_name, false));
+            if (!client) {
+                ROS_WARN("Failed to reinstantiate action client '%s'", action_name.c_str());
+                return false;
+            }
+        }
+
+        if (client->isServerConnected()) {
+            return true;
+        }
+
+        poll_frequency.sleep();
+
+        ROS_DEBUG("Waited %0.3f seconds for action server '%s'...", (ros::Time::now() - start).toSec(), action_name.c_str());
+    }
+
+    return false;
+}
 } // namespace rcta
 
 #endif
