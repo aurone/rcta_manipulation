aaca5cdca60a4236775d00ff3841556b25d0708a
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-03-26 11:36:31 -0400

    setup object_detection and hdt_manipulation packages

--- object_detection/src/pick_and_place_node/hdt_pick_and_place.cpp
+++ object_detection/src/pick_and_place_node/hdt_pick_and_place.cpp
@@ -1,8 +1,74 @@
-#include "PickAndPlaceNode.h"
+#include <ros/ros.h>
+
+// standard includes
+#include <cstdio>
+#include <memory>
+#include <string>
+#include <vector>
 
 // system includes
+#include <actionlib/server/simple_action_server.h>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <message_filters/subscriber.h>
+#include <pr2_vfh_database/ObjectFinder.h>
+#include <pr2_vfh_database/pr2_grasp_database.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/message_filter.h>
+#include <tf/transform_listener.h>
 #include <visualization_msgs/MarkerArray.h>
+
+// project includes
+#include <rcta/ObjectDetectionAction.h>
+
+/// Basic node that subscribes to point clouds and returns the best match in the
+/// given object database for each received point cloud.
+class PickAndPlaceNode
+{
+public:
+
+    PickAndPlaceNode();
+
+    bool initialize();
+    int run();
+
+private:
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle ph_;
+
+    typedef actionlib::SimpleActionServer<rcta::ObjectDetectionAction> ObjectDetectionActionServer;
+    std::unique_ptr<ObjectDetectionActionServer> action_server_;
+
+    std::string point_cloud_topic_;
+    // message_filters::Subscriber<sensor_msgs::PointCloud2> point_cloud_sub_;
+    // std::shared_ptr<tf::MessageFilter<sensor_msgs::PointCloud2>> tf_filter_;
+    ros::Subscriber point_cloud_sub_;
+
+    std::unique_ptr<ObjectFinder> object_detector_;
+
+    sensor_msgs::PointCloud2::ConstPtr last_point_cloud_;
+    tf::TransformListener listener_;
+
+    std::string database_directory_;
+
+    std::string last_database_filename_;
+    std::string last_features_filename_;
+    std::string last_kdtree_indices_filename_;
+
+    ros::Publisher pregrasp_pub_;
+
+    rcta::ObjectDetectionFeedback feedback_;
+    rcta::ObjectDetectionResult result_;
+
+    void point_cloud_callback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+
+    void print_database_contents(const std::string& database_filename) const;
+
+    void object_detection_callback(const rcta::ObjectDetectionGoal::ConstPtr& goal);
+
+    void publish_grasp_markers(const geometry_msgs::PoseArray& grasps, const geometry_msgs::PoseArray& pregrasps) const;
+};
+
 
 PickAndPlaceNode::PickAndPlaceNode() :
     nh_(),
@@ -218,4 +284,16 @@ void PickAndPlaceNode::publish_grasp_markers(const geometry_msgs::PoseArray& gra
     }
 
     pregrasp_pub_.publish(arr);
+}
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "hdt_pick_and_place");
+
+    PickAndPlaceNode node;
+    if (!node.initialize()) {
+        ROS_ERROR("Failed to initialize Pick And Place Node");
+        return 1;
+    }
+    return node.run();
 }
