e341946add0d9739e312f548a91469baf2a5d605
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-08-14 16:31:53 -0400

    beef up assertion macro

--- object_manipulation_planner/src/assert.h
+++ object_manipulation_planner/src/assert.h
@@ -1,9 +1,45 @@
-#ifndef OBJECT_MANIPULATION_PLANNER_ASSERT_H
-#define OBJECT_MANIPULATION_PLANNER_ASSERT_H
+#ifndef SMPL_ASSERT_H
+#define SMPL_ASSERT_H
 
-#include <exception>
+#include <cstdio>
+#include <cstdlib>
 
-#define SMPL_ASSERT(cond, msg) do { if (!(cond)) throw std::runtime_error(msg); } while (0)
+#define SMPL_ASSERT_LEVEL_NONE      0   // compile out all assertions
+#define SMPL_ASSERT_LEVEL_STRICT    1   // compile in SMPL_ASSERT_STRICT
+#define SMPL_ASSERT_LEVEL_NORMAL    2   // compile in SMPL_ASSERT and SMPL_ASSERT_STRICT
+#define SMPL_ASSERT_LEVEL_DEBUG     3   // compile in SMPL_ASSERT_DEBUG, SMPL_ASSERT, SMPL_ASSERT_STRICT
+
+#ifndef SMPL_ASSERT_LEVEL
+#define SMPL_ASSERT_LEVEL SMPL_ASSERT_LEVEL_NORMAL
+#endif
+
+#define SMPL_ASSERT_AUX(cond, msg) \
+    do { \
+        if (!(cond)) { \
+            ::std::fprintf(stderr, "%s::%d: Assertion `%s` failed.\n", __FILE__, __LINE__, #cond); \
+            ::std::abort(); \
+        } \
+    } while (0)
+
+#define SMPL_ASSERT_AUX_EMPTY(cond, msg) do { (void)sizeof((cond)); } while (0)
+
+#if SMPL_ASSERT_LEVEL >= SMPL_ASSERT_LEVEL_DEBUG
+#define SMPL_ASSERT_DEBUG(cond, msg) SMPL_ASSERT_AUX(cond, msg)
+#else
+#define SMPL_ASSERT_DEBUG(cond, msg) SMPL_ASSERT_AUX_EMPTY(cond, msg)
+#endif
+
+#if SMPL_ASSERT_LEVEL >= SMPL_ASSERT_LEVEL_NORMAL
+#define SMPL_ASSERT(cond, msg) SMPL_ASSERT_AUX(cond, msg)
+#else
+#define SMPL_ASSERT(cond, msg) SMPL_ASSERT_AUX_EMPTY(cond, msg)
+#endif
+
+#if SMPL_ASSERT_LEVEL >= SMPL_ASSERT_LEVEL_STRICT
+#define SMPL_ASSERT_STRICT(cond, msg) SMPL_ASSERT_AUX(cond, msg)
+#else
+#define SMPL_ASSERT_STRICT(cond, msg) SMPL_ASSERT_AUX_EMPTY(cond, msg)
+#endif
 
 #endif
 
