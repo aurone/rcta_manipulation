a491788bc70d8887926247f501e5450fec560c39
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-08-15 13:57:23 -0400

    remove some verbose marker debugging

--- object_manipulation_planner/src/object_manip_planner_node.cpp
+++ object_manipulation_planner/src/object_manip_planner_node.cpp
@@ -234,11 +234,6 @@ int main(int argc, char* argv[])
                 color,
                 "start_state",
                 ros::Duration(0.0));
-        for (auto& marker : ma.markers) {
-            ROS_INFO("%s", marker.header.frame_id.c_str());
-            ROS_INFO("%f, %f, %f", marker.scale.x, marker.scale.y, marker.scale.z);
-            ROS_INFO("color = %f, %f, %f, %f", marker.color.r, marker.color.g, marker.color.b,  marker.color.a);
-        }
         ROS_INFO("Visualize %zu markers", ma.markers.size());
         SV_SHOW_INFO(ma);
         ros::Duration(1.0).sleep();
