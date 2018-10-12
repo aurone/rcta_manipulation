346cb281ae9076479d9a8f68fea137313dfce23f
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-10-11 23:03:10 -0400

    read in start state parameter in manipulate object test

--- object_manipulation_planner/src/manipulate_object.cpp
+++ object_manipulation_planner/src/manipulate_object.cpp
@@ -18,7 +18,9 @@ int main(int argc, char* argv[])
     ros::Duration(1.0).sleep();
 
     std::map<std::string, double> start_variables;
-    (void)ph.getParam("start_variables", start_variables);
+    (void)ph.getParam("start_state", start_variables);
+
+    std::vector<std::pair<std::string, double>> wtf;
 
     for (auto& var : start_variables) {
         auto vv = var.first;
@@ -30,6 +32,8 @@ int main(int argc, char* argv[])
         if (vv != "world_joint/x" && vv != "world_joint/y") {
             value = smpl::to_radians(value);
         }
+
+        wtf.emplace_back(vv, value);
     }
 
     auto object_start_state = 0.0;
@@ -64,47 +68,34 @@ int main(int argc, char* argv[])
 
     goal.start_state.is_diff = true;
 
-    goal.start_state.joint_state.name =
-    {
-        "limb_right_joint1",
-        "limb_right_joint2",
-        "limb_right_joint3",
-        "limb_right_joint4",
-        "limb_right_joint5",
-        "limb_right_joint6",
-        "limb_right_joint7",
-        "limb_left_joint1",
-        "limb_left_joint2",
-        "limb_left_joint3",
-        "limb_left_joint4",
-        "limb_left_joint5",
-        "limb_left_joint6",
-        "limb_left_joint7",
-    };
-
-    goal.start_state.joint_state.position =
-    {
-        smpl::to_radians(135.0),
-        smpl::to_radians(0.0),
-        smpl::to_radians(180.0),
-        smpl::to_radians(45.0),
-        smpl::to_radians(30.0),
-        smpl::to_radians(90.0),
-        smpl::to_radians(-135.0),
-        smpl::to_radians(-90.0),
-        smpl::to_radians(90.0),
-        smpl::to_radians(90.0),
-        smpl::to_radians(180.0),
-        smpl::to_radians(-90.0),
-        smpl::to_radians(0.0),
-        smpl::to_radians(0.0),
-    };
-
     goal.start_state.multi_dof_joint_state.header.frame_id = "map";
     goal.start_state.multi_dof_joint_state.joint_names = { "world_joint" };
 
     goal.start_state.multi_dof_joint_state.transforms.resize(1);
     goal.start_state.multi_dof_joint_state.transforms[0].rotation.w = 1.0;
+
+    for (auto& var : wtf) {
+        auto pos = var.first.find("world_joint");
+        if (pos == std::string::npos) {
+            goal.start_state.joint_state.name.push_back(var.first);
+            goal.start_state.joint_state.position.push_back(var.second);
+        } else {
+            auto local = var.first.substr(strlen("world_joint"));
+            if (local == "/x") {
+                goal.start_state.multi_dof_joint_state.transforms[0].translation.x = var.second;
+            } else if (local == "/y") {
+                goal.start_state.multi_dof_joint_state.transforms[0].translation.y = var.second;
+            } else if (local == "/theta") {
+                Eigen::Quaterniond q(Eigen::AngleAxisd(var.second, Eigen::Vector3d::UnitZ()));
+                goal.start_state.multi_dof_joint_state.transforms[0].rotation.w = q.w();
+                goal.start_state.multi_dof_joint_state.transforms[0].rotation.x = q.x();
+                goal.start_state.multi_dof_joint_state.transforms[0].rotation.y = q.y();
+                goal.start_state.multi_dof_joint_state.transforms[0].rotation.z = q.z();
+            }
+        }
+    }
+
+    client.waitForServer();
 
     auto state = client.sendGoalAndWait(goal);
     if (state.state_ == state.SUCCEEDED) {
