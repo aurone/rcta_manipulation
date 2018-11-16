c6351107c536bf266dcf3d4d1a2d3f6cb8435b18
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-11-16 18:40:07 -0500

    some robustness, refactoring, and add release-object motion

--- object_manipulation_planner/src/simple_crate_planner.cpp
+++ object_manipulation_planner/src/simple_crate_planner.cpp
@@ -16,6 +16,70 @@
 #include <smpl_urdf_robot_model/robot_state.h>
 #include <urdf_parser/urdf_parser.h>
 
+static
+auto to_cstring(moveit_msgs::MoveItErrorCodes code) -> const char*
+{
+    switch (code.val) {
+    case moveit_msgs::MoveItErrorCodes::SUCCESS:
+        return "SUCCESS";
+    case moveit_msgs::MoveItErrorCodes::FAILURE:
+        return "FAILURE";
+
+    case moveit_msgs::MoveItErrorCodes::PLANNING_FAILED:
+        return "PLANNING_FAILED";
+    case moveit_msgs::MoveItErrorCodes::INVALID_MOTION_PLAN:
+        return "INVALID_MOTION_PLAN";
+    case moveit_msgs::MoveItErrorCodes::MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE:
+        return "MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE";
+    case moveit_msgs::MoveItErrorCodes::CONTROL_FAILED:
+        return "CONTROL_FAILED";
+    case moveit_msgs::MoveItErrorCodes::UNABLE_TO_AQUIRE_SENSOR_DATA:
+        return "UNABLE_TO_AQUIRE_SENSOR_DATA";
+    case moveit_msgs::MoveItErrorCodes::TIMED_OUT:
+        return "TIMED_OUT";
+    case moveit_msgs::MoveItErrorCodes::PREEMPTED:
+        return "PREEMPTED";
+
+    case moveit_msgs::MoveItErrorCodes::START_STATE_IN_COLLISION:
+        return "START_STATE_IN_COLLISION";
+    case moveit_msgs::MoveItErrorCodes::START_STATE_VIOLATES_PATH_CONSTRAINTS:
+        return "START_STATE_VIOLATES_PATH_CONSTRAINTS";
+
+    case moveit_msgs::MoveItErrorCodes::GOAL_IN_COLLISION:
+        return "GOAL_IN_COLLISION";
+    case moveit_msgs::MoveItErrorCodes::GOAL_VIOLATES_PATH_CONSTRAINTS:
+        return "GOAL_VIOLATES_PATH_CONSTRAINTS";
+    case moveit_msgs::MoveItErrorCodes::GOAL_CONSTRAINTS_VIOLATED:
+        return "GOAL_CONSTRAINTS_VIOLATED";
+
+    case moveit_msgs::MoveItErrorCodes::INVALID_GROUP_NAME:
+        return "INVALID_GROUP_NAME";
+    case moveit_msgs::MoveItErrorCodes::INVALID_GOAL_CONSTRAINTS:
+        return "INVALID_GOAL_CONSTRAINTS";
+    case moveit_msgs::MoveItErrorCodes::INVALID_ROBOT_STATE:
+        return "INVALID_ROBOT_STATE";
+    case moveit_msgs::MoveItErrorCodes::INVALID_LINK_NAME:
+        return "INVALID_LINK_NAME";
+    case moveit_msgs::MoveItErrorCodes::INVALID_OBJECT_NAME:
+        return "INVALID_OBJECT_NAME";
+
+    case moveit_msgs::MoveItErrorCodes::FRAME_TRANSFORM_FAILURE:
+        return "FRAME_TRANSFORM_FAILURE";
+    case moveit_msgs::MoveItErrorCodes::COLLISION_CHECKING_UNAVAILABLE:
+        return "COLLISION_CHECKING_UNAVAILABLE";
+    case moveit_msgs::MoveItErrorCodes::ROBOT_STATE_STALE:
+        return "ROBOT_STATE_STALE";
+    case moveit_msgs::MoveItErrorCodes::SENSOR_INFO_STALE:
+        return "SENSOR_INFO_STALE";
+
+    case moveit_msgs::MoveItErrorCodes::NO_IK_SOLUTION:
+        return "NO_IK_SOLUTION";
+
+    default:
+        return "UNRECOGNIZED";
+    }
+}
+
 using ManipulateObjectActionServer =
         actionlib::SimpleActionServer<cmu_manipulation_msgs::ManipulateObjectAction>;
 
@@ -41,6 +105,7 @@ void reverse(Range&& range)
 
 bool OpenGripper(GripperCommandActionClient* gripper_client)
 {
+    ROS_INFO("Open gripper");
     control_msgs::GripperCommandGoal gripper_goal;
     gripper_goal.command.position = 0.0841;
     auto res = gripper_client->sendGoalAndWait(gripper_goal);
@@ -49,6 +114,7 @@ bool OpenGripper(GripperCommandActionClient* gripper_client)
 
 bool CloseGripper(GripperCommandActionClient* gripper_client)
 {
+    ROS_INFO("Close gripper");
     control_msgs::GripperCommandGoal gripper_goal;
     gripper_goal.command.position = 0.0;
     auto res = gripper_client->sendGoalAndWait(gripper_goal);
@@ -61,9 +127,155 @@ auto interp(const T& src, const T& dst, double t) -> T
     return (1.0 - t) * src + t * dst;
 }
 
+bool MoveCartesian(
+    moveit::planning_interface::MoveGroupInterface* move_group,
+    const Eigen::Affine3d& dst_pose,
+    const std::string& group_name,
+    const std::string& link_name,
+    double pct_thresh = 1.0)
+{
+    auto src_pose = move_group->getCurrentState()->getGlobalLinkTransform(link_name);
+
+    std::vector<geometry_msgs::Pose> waypoints;
+    waypoints.resize(1);
+    tf::poseEigenToMsg(dst_pose, waypoints[0]);
+    auto eef_step = 0.02;
+    auto jump_thresh = 10.0;
+    auto traj = moveit_msgs::RobotTrajectory();
+    auto avoid_collisions = true;
+    auto err = moveit::planning_interface::MoveItErrorCode();
+    auto pct = move_group->computeCartesianPath(
+            waypoints, eef_step, jump_thresh, traj, avoid_collisions, &err);
+    if (err != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
+        ROS_ERROR("Failed to plan cartesian-style!");
+        return false;
+    }
+
+    ROS_INFO("Cartesian move at %f%%", pct);
+
+    if (pct < pct_thresh) {
+        ROS_ERROR("Failed to compute cartesian motion (target percent = %f, achieved percent = %f)", pct_thresh, pct);
+        return false;
+    }
+
+    auto curr_state = move_group->getCurrentState();
+    if (curr_state == NULL) {
+        ROS_INFO("Failed to retrieve current state");
+        return false;
+    }
+
+    ///////////////////////
+    // Create trajectory //
+    ///////////////////////
+
+    auto ugh_traj = robot_trajectory::RobotTrajectory(
+            move_group->getRobotModel(), group_name);
+    ugh_traj.setRobotTrajectoryMsg(*curr_state, traj);
+
+    trajectory_processing::IterativeParabolicTimeParameterization itp;
+    if (!itp.computeTimeStamps(ugh_traj)) {
+        ROS_ERROR("Failed to compute timestamps");
+        return false;
+    }
+
+    ROS_INFO("computed time stamps");
+
+    /////////////////////
+    // Convert to Plan //
+    /////////////////////
+
+    auto plan = moveit::planning_interface::MoveGroupInterface::Plan();
+    robotStateToRobotStateMsg(*curr_state, plan.start_state_);
+    ugh_traj.getRobotTrajectoryMsg(plan.trajectory_);
+
+    for (auto& point : plan.trajectory_.joint_trajectory.points) {
+        ROS_INFO_STREAM(point.time_from_start.toSec() << ": " << point.positions);
+    }
+
+    for (auto& point : plan.trajectory_.joint_trajectory.points) {
+        point.time_from_start += ros::Duration(0.5);
+    }
+
+    ////////////////////
+    // Execute motion //
+    ////////////////////
+
+    err = move_group->execute(plan);
+    if (err != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
+        ROS_ERROR("Failed to move cartesian-style (%s)!", to_cstring(err));
+        return false;
+    }
+
+    return true;
+}
+
+bool MoveRelativeCartesian(
+    moveit::planning_interface::MoveGroupInterface* move_group,
+    const Eigen::Affine3d& dst_pose,
+    const std::string& group_name,
+    const std::string& link_name,
+    double pct_thresh = 1.0)
+{
+    auto src_pose = move_group->getCurrentState()->getGlobalLinkTransform(link_name);
+    auto tgt_pose = src_pose * dst_pose;
+    return MoveCartesian(move_group, tgt_pose, group_name, link_name, pct_thresh);
+}
+
+template <class Sampler>
+bool PlanManipulationTrajectory(
+    moveit::planning_interface::MoveGroupInterface* move_group,
+    const std::string& group_name,
+    const std::string& tool_link_name,
+    int samples,
+    Sampler sampler,
+    moveit::planning_interface::MoveGroupInterface::Plan* oplan)
+{
+    auto interm_state = *move_group->getCurrentState();
+    auto manip_traj = robot_trajectory::RobotTrajectory(interm_state.getRobotModel(), group_name);
+    auto ids = (int32_t)0;
+
+    for (auto i = 0; i < samples; ++i) {
+        auto alpha = (double)i / (double)(samples - 1);
+        auto contact_pose = sampler(alpha);
+
+        auto robot_model = interm_state.getRobotModel();
+        auto* group = robot_model->getJointModelGroup(group_name);
+
+        if (interm_state.setFromIK(group, contact_pose, tool_link_name)) {
+            visualization_msgs::MarkerArray ma;
+            std_msgs::ColorRGBA color;
+            color.r = 1.0f;
+            color.g = 0.5f;
+            color.b = 0.0f;
+            color.a = 0.8f;
+            interm_state.getRobotMarkers(ma, robot_model->getLinkModelNames(), color, "constrained", ros::Duration(0));
+            for (auto& m : ma.markers) {
+                m.id = ids++;
+            }
+            SV_SHOW_INFO(ma);
+            manip_traj.addSuffixWayPoint(interm_state, 1.0);
+        }
+    }
+
+    // timestamp waypoint
+    trajectory_processing::IterativeParabolicTimeParameterization itp;
+    if (!itp.computeTimeStamps(manip_traj)) {
+        ROS_ERROR("Failed to compute timestamps");
+        return false;
+    }
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    robotStateToRobotStateMsg(*move_group->getCurrentState(), plan.start_state_);
+    manip_traj.getRobotTrajectoryMsg(plan.trajectory_);
+
+    *oplan = std::move(plan);
+    return true;
+}
+
 bool ManipulateObject(
     const cmu_manipulation_msgs::ManipulateObjectGoal* goal,
     moveit::planning_interface::MoveGroupInterface* move_group,
+    const std::string* group_name,
     GripperCommandActionClient* gripper_client,
     const smpl::urdf::RobotModel* object_model)
 {
@@ -161,14 +373,16 @@ bool ManipulateObject(
     print_pose("grasp pose", contact_pose);
 
     // pre-grasp = straight back by some offset
-    auto pregrasp_offset = -0.15;
+    auto pregrasp_offset = -0.25;
     auto pregrasp_pose = smpl::Affine3(
             contact_pose *
             smpl::Translation3(pregrasp_offset, 0.0, 0.0));
 
     print_pose("pregrasp pose", pregrasp_pose);
 
-    move_group->setEndEffectorLink("limb_right_tool0");
+    auto tool_link_name = "limb_right_tool0";
+
+    move_group->setEndEffectorLink(tool_link_name);
 
     ////////////////////////////
     // Move to pre-grasp pose //
@@ -179,7 +393,7 @@ bool ManipulateObject(
         move_group->setGoalPositionTolerance(0.02);
         move_group->setGoalOrientationTolerance(smpl::to_radians(2.0));
 
-        move_group->setPoseTarget(pregrasp_pose, "limb_right_tool0");
+        move_group->setPoseTarget(pregrasp_pose, tool_link_name);
         auto err = move_group->move();
     }
 
@@ -196,65 +410,8 @@ bool ManipulateObject(
     // move to grasp //
     ///////////////////
 
-    {
-        ROS_INFO("pose reference frame: %s", move_group->getPoseReferenceFrame().c_str());
-        std::vector<geometry_msgs::Pose> waypoints;
-        waypoints.resize(2);
-        tf::poseEigenToMsg(pregrasp_pose, waypoints[0]);
-        tf::poseEigenToMsg(contact_pose, waypoints[1]);
-        auto eef_step = 0.02;
-        auto jump_thresh = 10.0;
-        auto traj = moveit_msgs::RobotTrajectory();
-        auto avoid_collisions = true;
-        auto err = moveit::planning_interface::MoveItErrorCode();
-        auto pct = move_group->computeCartesianPath(
-                waypoints, eef_step, jump_thresh, traj, avoid_collisions, &err);
-        if (err != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
-            ROS_ERROR("Failed to plan cartesian-style!");
-            return false;
-        }
-        ROS_INFO("Cartesian move at %f%%", pct);
-
-        if (curr_state == NULL) {
-            ROS_INFO("Failed to retrieve current state");
-            return false;
-        }
-
-        auto ugh_traj = robot_trajectory::RobotTrajectory(
-                move_group->getRobotModel(), "right_arm_and_torso");
-        ugh_traj.setRobotTrajectoryMsg(*curr_state, traj);
-
-        trajectory_processing::IterativeParabolicTimeParameterization itp;
-        if (!itp.computeTimeStamps(ugh_traj)) {
-            ROS_ERROR("Failed to compute timestamps");
-            return false;
-        }
-
-        ROS_INFO("computed time stamps");
-
-        auto plan = moveit::planning_interface::MoveGroupInterface::Plan();
-        robotStateToRobotStateMsg(*curr_state, plan.start_state_);
-        ugh_traj.getRobotTrajectoryMsg(plan.trajectory_);
-
-        if (!plan.trajectory_.joint_trajectory.points.empty()) {
-            reverse(plan.trajectory_.joint_trajectory.points);
-            plan.trajectory_.joint_trajectory.points.pop_back();
-            reverse(plan.trajectory_.joint_trajectory.points);
-        }
-        if (!plan.trajectory_.multi_dof_joint_trajectory.points.empty()) {
-            reverse(plan.trajectory_.multi_dof_joint_trajectory.points);
-            plan.trajectory_.multi_dof_joint_trajectory.points.pop_back();
-            reverse(plan.trajectory_.multi_dof_joint_trajectory.points);
-        }
-        for (auto& point : plan.trajectory_.joint_trajectory.points) {
-            ROS_INFO_STREAM(point.time_from_start.toSec() << ": " << point.positions);
-        }
-
-        err = move_group->execute(plan);
-        if (err != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
-            ROS_ERROR("Failed to move cartesian-style (%d)!", (int)err.val);
-            return false;
-        }
+    if (!MoveCartesian(move_group, contact_pose, *group_name, tool_link_name, 0.9)) {
+        return false;
     }
 
     ///////////////////////
@@ -271,67 +428,46 @@ bool ManipulateObject(
     ///////////////////////////
 
     {
-        auto interm_state = *move_group->getCurrentState();
-        auto manip_traj = robot_trajectory::RobotTrajectory(interm_state.getRobotModel(), "right_arm_and_torso");
-        auto ids = (int32_t)0;
-
-        auto res = 0.01; // centimeters
-        // s = r*theta;
-        auto theta = std::fabs(get_lid_pos_from_pct(goal->object_goal) - get_lid_pos_from_pct(goal->object_start));
-        auto radius = 0.15;
-        auto arc = theta * radius;
-        ROS_INFO("arc = %f", arc);
-
-        // at least two samples
-        auto samples = std::max(2, (int)std::round(arc / res));
-        ROS_INFO("samples = %d", samples);
-
-        for (auto i = 0; i < samples; ++i) {
-            auto alpha = (double)i / (double)(samples - 1);
-            auto s = interp(get_lid_pos_from_pct(goal->object_start), get_lid_pos_from_pct(goal->object_goal), alpha);
-            ROS_INFO("set lid to %f", s);
+        auto sample_manifold = [&](double alpha) -> Eigen::Affine3d
+        {
+            auto s = interp(
+                    get_lid_pos_from_pct(goal->object_start),
+                    get_lid_pos_from_pct(goal->object_goal),
+                    alpha);
+            ROS_DEBUG("set lid to %f", s);
 
             SetVariablePosition(&object_state, lid_var, s);
 
             auto* contact_link = GetLink(object_model, "tool");
             if (contact_link == NULL) {
                 ROS_ERROR("No 'tool' link");
-                return false;
+                return Eigen::Affine3d::Identity();
             }
 
             auto contact_pose = *GetUpdatedLinkTransform(&object_state, contact_link);
-            // rotate so that the end effector is correct
             contact_pose *= smpl::AngleAxis(0.5 * M_PI, smpl::Vector3::UnitX());
+            return contact_pose;
+        };
 
-            auto robot_model = interm_state.getRobotModel();
-            auto* group = robot_model->getJointModelGroup("right_arm_and_torso");
+        auto theta = std::fabs(
+                get_lid_pos_from_pct(goal->object_goal) -
+                get_lid_pos_from_pct(goal->object_start));
+        auto radius = 0.15;
+        auto arc = theta * radius; // s = r*theta;
+        ROS_INFO("arc = %f", arc);
 
-            if (interm_state.setFromIK(group, contact_pose, "limb_right_tool0")) {
-                visualization_msgs::MarkerArray ma;
-                std_msgs::ColorRGBA color;
-                color.r = 1.0f;
-                color.g = 0.5f;
-                color.b = 0.0f;
-                color.a = 0.8f;
-                interm_state.getRobotMarkers(ma, robot_model->getLinkModelNames(), color, "constrained", ros::Duration(0));
-                for (auto& m : ma.markers) {
-                    m.id = ids++;
-                }
-                SV_SHOW_INFO(ma);
-                manip_traj.addSuffixWayPoint(interm_state, 1.0);
-            }
-        }
+        auto res = 0.01; // centimeters
+        auto samples = std::max(2, (int)std::round(arc / res));
 
-        // timestamp waypoint
-        trajectory_processing::IterativeParabolicTimeParameterization itp;
-        if (!itp.computeTimeStamps(manip_traj)) {
-            ROS_ERROR("Failed to compute timestamps");
+        auto plan = moveit::planning_interface::MoveGroupInterface::Plan();
+        if (!PlanManipulationTrajectory(
+                move_group,
+                *group_name, tool_link_name,
+                samples, sample_manifold,
+                &plan))
+        {
             return false;
         }
-
-        moveit::planning_interface::MoveGroupInterface::Plan plan;
-        robotStateToRobotStateMsg(*move_group->getCurrentState(), plan.start_state_);
-        manip_traj.getRobotTrajectoryMsg(plan.trajectory_);
 
         auto err = move_group->execute(plan);
         if (err != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
@@ -347,6 +483,17 @@ bool ManipulateObject(
     if (!OpenGripper(gripper_client)) {
         ROS_ERROR("Failed to open gripper");
         return false;
+    }
+
+    //////////////////////////////
+    // move to pre-release pose //
+    //////////////////////////////
+
+    {
+        auto curr_state = *move_group->getCurrentState();
+        auto& tool_transform = curr_state.getGlobalLinkTransform(tool_link_name);
+        auto prerelease_pose = tool_transform * Eigen::Translation3d(-0.15, 0.0, 0.0);
+        MoveCartesian(move_group, prerelease_pose, *group_name, tool_link_name, 0.0);
     }
 
     //////////////////////////////////
@@ -415,8 +562,9 @@ int main(int argc, char* argv[])
     // Initialize Move Group Interface //
     /////////////////////////////////////
 
-    auto move_group = moveit::planning_interface::MoveGroupInterface(
-            "right_arm_and_torso");
+    auto group_name = std::string("right_arm_and_torso");
+    auto move_group =
+            moveit::planning_interface::MoveGroupInterface(group_name);
 
     // TODO: ugh of course this is only settable in the planning frame
     move_group.setWorkspace(-0.5, -1.5, -0.2, 1.5, 1.5, 1.8);
@@ -441,7 +589,7 @@ int main(int argc, char* argv[])
             "manipulate_object",
             [&](const cmu_manipulation_msgs::ManipulateObjectGoal::ConstPtr& msg)
             {
-                if (!ManipulateObject(msg.get(), &move_group, &gripper_client, &object_model)) {
+                if (!ManipulateObject(msg.get(), &move_group, &group_name, &gripper_client, &object_model)) {
                     ROS_ERROR("Action server aborted");
                     server.setAborted();
                 } else {
